5f3267a4f23e9fe2a9336dca012ec882fa307bf0
Mohammad Rafieefard <mrafieefard@gmail.com>
2025-03-03 17:47:31 +0100

    feat : add count client

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const baseUrl = import.meta.env.VITE_BACKEND_URL
 
+function App() {
+  const [count, setCount] = useState<number | undefined>(undefined)
+  useEffect(() => {
+    fetch(`${baseUrl}/count`, {
+    }).then((value) => {
+      value.json().then((value)=>{
+        setCount(value.count)
+      })
+    })
+  }, [])
   return (
     <>
       <div>
@@ -18,7 +27,17 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => {
+          fetch(`${baseUrl}/count`, {
+            method: "POST"
+          }).then((value) => {
+            value.json().then((value)=>{
+              setCount(value.count)
+            })
+            
+            
+          })
+        }}>
           count is {count}
         </button>
         <p>
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,15 @@
 /// <reference types="vite/client" />
+
+declare global {
+    interface ImportMetaEnv {
+        readonly VITE_BACKEND_HOSTNAME: string;
+        readonly VITE_BACKEND_PORT: string;
+        readonly VITE_BACKEND_URL: string;
+    }
+
+    interface ImportMeta {
+        readonly env: ImportMetaEnv;
+    }
+}
+
+export {} // Ensures this file is treated as a module
