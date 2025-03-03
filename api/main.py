249efed32b3e9751fb39f1c4782816515eac8c18
Mohammad Rafieefard <mrafieefard@gmail.com>
2025-03-03 16:28:44 +0100

    feat : add backend

---
+++ api/main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+import asyncio
+
+origins = [
+    "http://127.0.0.1",
+    "http://127.0.0.1:3000",
+]
+
+app = FastAPI(root_path="/api")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+async def run_fastapi():
+    
+    config = uvicorn.Config(app, host="0.0.0.0", port=8000, loop="asyncio")
+    server = uvicorn.Server(config)
+    await server.serve()
+
+def handle_exit(sig, loop):
+    loop.stop()
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(run_fastapi())       
+    except (KeyboardInterrupt,SystemExit):
+        print('INFO:     SHUTDOWN')
